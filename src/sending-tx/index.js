d16f3142cfac3bae62c9cb27532a0c8ef9fafad5
PanaW <wangpan@conflux-chain.org>
2021-07-16 16:42:51 +0800

    update tx example

--- src/sending-tx/index.js
+++ src/sending-tx/index.js
@@ -3,11 +3,11 @@ const cfx = new Conflux({
   url: 'https://test.confluxrpc.com',
   networkId: 1,
 });
+// NOTE: Replace with your own private key which have CFX balance
 const account = cfx.wallet.addPrivateKey(process.env.PRIVATE_KEY);
 const targetAddress = 'cfxtest:aame568esrpusxku1c449939ntrx2j0rxpmm5ge874';
 
 async function main() {
-  
   // check balance
   await getBalance(targetAddress);
   //
@@ -26,16 +26,17 @@ async function getBalance(address) {
   console.log(`Balance of ${address} is ${balanceInDrip.toCFX()} CFX`);
 }
 
+// Send a simple TX
 async function sendSimpleTx() {
   const hash = await cfx.sendTransaction({
     from: account.address,
     to: targetAddress,
     value: Drip.fromCFX(1.1),
   });
-  console.log(`TX hash ${hash}`);
   return hash;
 }
 
+// Sending a complete TX
 async function sendCompleteTX() {
   const currentEpoch = await cfx.getEpochNumber();
   const nonce = await cfx.getNextNonce(account.address);
@@ -52,10 +53,10 @@ async function sendCompleteTX() {
   txInfo.gas = estimate.gasLimit;
   txInfo.storageLimit = estimate.storageCollateralized;
   const hash = await cfx.sendTransaction(txInfo);
-  console.log(`TX hash ${hash}`);
   return hash;
 }
 
+// pendingTx have several helper methods that can get different stage tx data
 async function txStatusHelperMethods() {
   const pendingTx = cfx.sendTransaction({
     from: account.address,
@@ -63,19 +64,20 @@ async function txStatusHelperMethods() {
     value: Drip.fromCFX(1.1),
   });
   let hash = await pendingTx;
-  // let tx = await pendingTx.get();
-  // tx = await pendingTx.mined();
+  let tx = await pendingTx.get();
+  tx = await pendingTx.mined();
   let receipt = await pendingTx.executed();
   receipt = await pendingTx.confirmed();
   return hash;
 }
 
+// Demonstrate how to make sure a tx is confirmed
 async function checkConfirmed(hash) {
-  let confirmedEpoch = await cfx.getEpochNumber(CONST.EPOCH_NUMBER.LATEST_CONFIRMED);
   let receipt = await cfx.getTransactionReceipt(hash);
   if (!receipt) return;
-  // console.log("是否确认: ", receipt.epochNumber < confirmedEpoch, receipt.epochNumber - confirmedEpoch);
+  let confirmedEpoch = await cfx.getEpochNumber(CONST.EPOCH_NUMBER.LATEST_CONFIRMED);
+  console.log("Confirmed: ", receipt.epochNumber < confirmedEpoch, receipt.epochNumber - confirmedEpoch);
 
-  let risk = await cfx.getConfirmationRiskByHash(receipt.blockHash);
-  console.log(risk);
+  // let risk = await cfx.getConfirmationRiskByHash(receipt.blockHash);
+  // console.log(risk);
 }
